f049ccd1223656e05728edaeb741ccfb3734a287
Stanislav Hetzel <stanislav.hetzel@outlook.com>
2023-03-12 15:22:43 +0100

    Initialize

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, send_file, jsonify
+import io
+import torch
+from diffusers import StableDiffusionPipeline
+
+app = Flask(__name__)
+
+pipe = StableDiffusionPipeline.from_pretrained(
+    "XpucT/Deliberate"
+)
+
+pipe.enable_attention_slicing()
+
+@app.route('/generate', methods=['POST'])
+def generate_image():
+    prompt = request.json.get('prompt')
+    if not prompt:
+        return jsonify({'error': 'Prompt is required.'}), 400
+
+    image = pipe(
+        prompt=prompt,
+        width=768,
+        height=768,
+        num_inference_steps=25
+    ).images[0]
+    
+    img_io = io.BytesIO()
+    image.save(img_io, 'PNG')
+    img_io.seek(0)
+
+    return send_file(img_io, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(port=8080)
